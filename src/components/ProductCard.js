8f4f2391c634c10e257a42f33ecdb5a791a18dea
nurulamin092 <mnurulamin092@gmial.com>
2022-12-02 11:47:21 +0600

    product quantity added

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,16 +2,22 @@ import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
 import { addToCard, removeFormCart } from "../redux/actionCreators/productActions";
-import {RiDeleteBin4Fill, RiDeleteBin4Line} from 'react-icons/ri'
+import { RiDeleteBin4Line} from 'react-icons/ri'
 import { useLocation } from "react-router-dom";
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
   const {pathname} = useLocation();
   return (
+
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      className='shadow-lg rounded-3xl border relative  p-3 flex flex-col text-indigo-900'
       key={product._id}
     >
+      {pathname.includes('cart')&&<div className="grid place-items-center absolute top-2 right-2 bg-indigo-500 text-white h-8 w-8 rounded-full">
+          <p>
+             {product.quantity}
+          </p>
+      </div>}
       <div className='h-52 w-52 mx-auto'>
         <img src={product.image} alt={product.model} />
       </div>
